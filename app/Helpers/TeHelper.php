dbc72b62eedfcd0c4295176e20faf9675f001623
alan <ailustrisimo@simplehealthplans.com>
2018-03-12 13:25:27 +0800

    created the will expire at helper function

--- app/Helpers/TeHelper.php
+++ app/Helpers/TeHelper.php
@@ -1,7 +1,20 @@
 <?php 
 
 	if(!function_exists('willExpireAt'))	{
-		function willExpireAt()	{
-			
+
+		/**
+		 *	@param: $date_created the date the entity is created
+		 *
+		 *	@return: returns the number of days before the entity will expire
+		 */
+
+
+		function willExpireAt($date_ceated)	{
+			$current_time = time();		
+			$date_ceated = strtotime($date_ceated);
+			$time_diff = $current_time - $date_created;
+			$day_difference = $time_diff / (3600 * 24);
+
+			return intval(floor($day_difference) / 86400); //returns the nuumber of days before it will expire
 		}
 	}
